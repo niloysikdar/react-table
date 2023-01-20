f306a549322fc2897ccf33e05356fd2726df5119
Niloy Sikdar <niloysikdar30@gmail.com>
2023-01-21 05:16:26 +0530

    feat: day filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useMemo, useState } from 'react';
-import Box from '@mui/material/Box';
+import dayjs, { Dayjs } from 'dayjs';
 import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { Box } from '@mui/material';
 import SelectFilter from './components/SelectFilter';
 import OrderTable from './components/Table';
 import { orderData } from './data';
-import { filterData } from './utils/filterUtils';
+import { filterData, filterDataByDate } from './utils/filterUtils';
 
 const allStatus = ['All', ...new Set(orderData.map((e) => e.status))];
 const allSuppliers = ['All', ...new Set(orderData.map((e) => e.vendor))];
@@ -15,11 +20,23 @@ function App() {
     supplier: allSuppliers[0],
   });
 
-  const filteredData = useMemo(() => filterData(orderData, filters), [filters]);
+  const [fromDate, setFromDate] = useState<Dayjs | null>(null);
+  const [toDate, setToDate] = useState<Dayjs | null>(null);
+
+  const filteredData = useMemo(() => {
+    const data = filterData(orderData, filters);
+    return filterDataByDate(
+      data,
+      fromDate?.format('DD/MM/YYYY') || '',
+      toDate?.format('DD/MM/YYYY') || '',
+    );
+  }, [filters, fromDate, toDate]);
 
   return (
     <Box marginTop="2rem">
-      <Paper sx={{ display: 'flex', marginBottom: '2rem' }}>
+      <Paper
+        sx={{ display: 'flex', marginBottom: '2rem', alignItems: 'center' }}
+      >
         <SelectFilter
           id="status"
           label="Status"
@@ -35,6 +52,24 @@ function App() {
           options={allSuppliers}
           onChange={(e) => setFilters({ ...filters, supplier: e.target.value })}
         />
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            label="Select Date"
+            inputFormat="DD/MM/YYYY"
+            value={fromDate}
+            onChange={(newValue) => setFromDate(newValue)}
+            renderInput={(params) => <TextField size="small" {...params} />}
+          />
+
+          <DatePicker
+            label="Select Date"
+            inputFormat="DD/MM/YYYY"
+            value={toDate}
+            onChange={(newValue) => setToDate(newValue)}
+            renderInput={(params) => <TextField size="small" {...params} />}
+          />
+        </LocalizationProvider>
       </Paper>
 
       <OrderTable orderData={filteredData} />
--- src/utils/filterUtils.ts
+++ src/utils/filterUtils.ts
@@ -20,3 +20,82 @@ export function filterData(
     (item) => item.status === status && item.vendor === supplier,
   );
 }
+
+export function filterDataByDate(
+  data: OrderData[],
+  dateFrom: string,
+  dateTo: string,
+): OrderData[] | [] {
+  if (dateFrom === '' && dateTo === '') {
+    return data;
+  }
+
+  return data.filter((item) =>
+    dateCheck({ from: dateFrom, to: dateTo, check: item.dueDate }),
+  );
+}
+
+function dateCheck({
+  from,
+  to,
+  check,
+}: {
+  from?: string;
+  to?: string;
+  check: string;
+}): boolean {
+  const fromSplit = from?.split('/') || [];
+  const toSplit = to?.split('/') || [];
+  const checkSplit = check.split('/');
+  let from1, to1, check1;
+
+  if (fromSplit.length !== 3 && toSplit.length !== 3) {
+    return true;
+  }
+
+  if (fromSplit.length === 3 && toSplit.length !== 3) {
+    from1 = new Date(
+      parseInt(fromSplit[2]),
+      parseInt(fromSplit[1]) - 1,
+      parseInt(fromSplit[0]),
+    ); // -1 because months are from 0 to 11
+    check1 = new Date(
+      parseInt(checkSplit[2]),
+      parseInt(checkSplit[1]) - 1,
+      parseInt(checkSplit[0]),
+    );
+    return check1 >= from1;
+  }
+
+  if (fromSplit.length !== 3 && toSplit.length === 3) {
+    to1 = new Date(
+      parseInt(toSplit[2]),
+      parseInt(toSplit[1]) - 1,
+      parseInt(toSplit[0]),
+    );
+    check1 = new Date(
+      parseInt(checkSplit[2]),
+      parseInt(checkSplit[1]) - 1,
+      parseInt(checkSplit[0]),
+    );
+    return check1 <= to1;
+  }
+
+  from1 = new Date(
+    parseInt(fromSplit[2]),
+    parseInt(fromSplit[1]) - 1,
+    parseInt(fromSplit[0]),
+  ); // -1 because months are from 0 to 11
+  to1 = new Date(
+    parseInt(toSplit[2]),
+    parseInt(toSplit[1]) - 1,
+    parseInt(toSplit[0]),
+  );
+  check1 = new Date(
+    parseInt(checkSplit[2]),
+    parseInt(checkSplit[1]) - 1,
+    parseInt(checkSplit[0]),
+  );
+
+  return check1 >= from1 && check1 <= to1;
+}
