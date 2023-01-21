56bafefadec20503628e94046438aedb05c6abd9
Niloy Sikdar <niloysikdar30@gmail.com>
2023-01-22 03:25:22 +0530

    feat: code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { lazy, Suspense } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Box } from '@mui/material';
-import FiltersBar from './components/FiltersBar';
-import SearchBar from './components/SearchBar';
-import OrderTable from './components/Table';
+import CircularProgress from '@mui/material/CircularProgress';
+const FiltersBar = lazy(() => import('./components/FiltersBar'));
+const SearchBar = lazy(() => import('./components/SearchBar'));
+const OrderTable = lazy(() => import('./components/Table'));
 
 const queryClient = new QueryClient();
 
@@ -12,12 +14,52 @@ function App() {
       padding="2rem 1rem"
       sx={{ minHeight: '100vh', backgroundColor: '#f3f4f6' }}
     >
-      <SearchBar />
-      <FiltersBar />
+      <Suspense
+        fallback={
+          <Box
+            padding="2rem"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <CircularProgress />
+          </Box>
+        }
+      >
+        <SearchBar />
+      </Suspense>
 
-      <QueryClientProvider client={queryClient}>
-        <OrderTable />
-      </QueryClientProvider>
+      <Suspense
+        fallback={
+          <Box
+            padding="2rem"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <CircularProgress />
+          </Box>
+        }
+      >
+        <FiltersBar />
+      </Suspense>
+
+      <Suspense
+        fallback={
+          <Box
+            padding="2rem"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <CircularProgress />
+          </Box>
+        }
+      >
+        <QueryClientProvider client={queryClient}>
+          <OrderTable />
+        </QueryClientProvider>
+      </Suspense>
     </Box>
   );
 }
